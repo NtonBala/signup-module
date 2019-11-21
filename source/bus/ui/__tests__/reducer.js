bbaae5312b6d46153b31079f4f54c37b013f517c
Anton Balashov <balashov.anton.1983@gmail.com>
2019-11-22 01:30:35 +0200

    Fix jest & babel 7 configuration issue

--- source/bus/ui/__tests__/reducer.js
+++ source/bus/ui/__tests__/reducer.js
@@ -6,14 +6,26 @@ import { uiActions } from "../actions";
 
 describe("ui reducer:", () => {
     test("should return initial state by default", () => {
-        expect(uiReducer(void 0, {})).toMatchInlineSnapshot();
+        expect(uiReducer(void 0, {})).toMatchInlineSnapshot(`
+      Object {
+        "isFetching": false,
+      }
+    `);
     });
 
     test("should handle START_FETCHING action", () => {
-        expect(uiReducer(void 0, uiActions.startFetching())).toMatchInlineSnapshot();
+        expect(uiReducer(void 0, uiActions.startFetching())).toMatchInlineSnapshot(`
+      Object {
+        "isFetching": true,
+      }
+    `);
     });
 
     test("should handle STOP_FETCHING action", () => {
-        expect(uiReducer(void 0, uiActions.stopFetching())).toMatchInlineSnapshot();
+        expect(uiReducer(void 0, uiActions.stopFetching())).toMatchInlineSnapshot(`
+      Object {
+        "isFetching": false,
+      }
+    `);
     });
 });
