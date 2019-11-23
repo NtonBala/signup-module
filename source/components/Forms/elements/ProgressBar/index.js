da0392cbe5dfcf9e7d4250a96bb07a49fb0531b4
Anton Balashov <balashov.anton.1983@gmail.com>
2019-11-23 02:46:11 +0200

    Animate progress bar

--- source/components/Forms/elements/ProgressBar/index.js
+++ source/components/Forms/elements/ProgressBar/index.js
@@ -1,14 +1,25 @@
 // Core
 import React from 'react';
 import { number } from 'prop-types';
+import cx from 'classnames';
 
 // Instruments
 import Styles from './styles.m.css';
 
 const ProgressBar = ({ step }) => {
+    console.log('-> step:', step);
+    const _getBarStyles = () => {
+        return cx(Styles.bar, {
+            [Styles.secondStep]: step === 2,
+            [Styles.thirdStep]:  step === 3,
+        });
+    };
+
+    const barStyles = _getBarStyles();
+
     return (
         <div className = { Styles.progress }>
-            <span />
+            <span className = { barStyles } />
         </div>
     );
 };
