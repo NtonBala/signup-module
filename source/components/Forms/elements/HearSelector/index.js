4763ba89b65f306d1e29133a09afff07ea1fc8ed
Anton Balashov <balashov.anton.1983@gmail.com>
2019-11-27 22:35:20 +0200

    Work in progress

--- source/components/Forms/elements/HearSelector/index.js
+++ source/components/Forms/elements/HearSelector/index.js
@@ -25,21 +25,82 @@ const options = [
     }
 ];
 
+const selectStyles = {
+    control: (styles) => {
+        return {
+            ...styles,
+            borderRadius: 3,
+            borderColor:  '#C4C4C4',
+            boxShadow:    'none',
+            '&:hover':    {
+                borderColor: '#C4C4C4',
+            },
+        };
+    },
+
+    singleValue: (styles) => {
+        return {
+            ...styles,
+            color:  '#5C5B65',
+            height: 16,
+        };
+    },
+
+    menu: (styles) => {
+        return {
+            ...styles,
+            borderRadius: 3,
+        };
+    },
+
+    option: (styles, { isDisabled, isSelected, isFocused }) => {
+        return {
+            ...styles,
+            backgroundColor: isDisabled
+                ? null
+                : isSelected
+                    ? '#1770CF'
+                    : isFocused
+                        ? '#A3CBF5'
+                        : null,
+            color: isSelected ? '#FFFFFF' : '#5C5B65',
+        };
+    },
+};
+
 const HearSelector = () => {
     return (
         <div className = { Styles.hearSelector }>
             <Field
                 component = { ({ input, meta: { touched, error, valid }}) => {
+                    const _onBlur = () => {
+                        input.onBlur(input.value);
+                    };
+
+                    const _onChange = (value) => {
+                        input.onChange(value);
+                    };
+
                     return (
                         <>
                             { valid && <label>where did you hear about us?</label> }
 
                             { touched && error && <span>{error}</span> }
 
-                            <Select
-                                { ...input }
-                                options = { options }
-                            />
+                            <div>
+                                <Select
+                                    { ...input }
+                                    isClearable
+                                    components = { {
+                                        IndicatorSeparator: () => null,
+                                    } }
+                                    options = { options }
+                                    placeholder = { null }
+                                    styles = { selectStyles }
+                                    onBlur = { _onBlur }
+                                    onChange = { _onChange }
+                                />
+                            </div>
                         </>
                     );
                 } }
