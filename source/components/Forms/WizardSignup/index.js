7778be0bd8e4d802af3312bac5b479e3dfeef6f4
Anton Balashov <balashov.anton.1983@gmail.com>
2019-12-15 02:43:55 +0200

    Animate WizardSignup

--- source/components/Forms/WizardSignup/index.js
+++ source/components/Forms/WizardSignup/index.js
@@ -4,6 +4,8 @@ import { func, shape } from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { change } from 'redux-form';
+import { Transition } from 'react-transition-group';
+import { gsap } from 'gsap/all';
 
 // Steps
 import FirstStep from './FirstStep';
@@ -19,33 +21,83 @@ import Styles from './styles.m.css';
 const WizardSignup = ({ actions, onSubmit }) => {
     const [step, setStep] = useState(1);
 
-    const stepForward = () => {
+    const timeout = {
+        enter: 600,
+        exit:  1000,
+    };
+
+    const _stepForward = () => {
         setStep(step + 1);
     };
 
-    const stepBackward = () => {
+    const _stepBackward = () => {
         setStep(step - 1);
     };
 
-    const titleJSX = step !== 3 ? <h3>Signup</h3> : <h3>Thank you!</h3>;
+    const _animateStepEnter = (el) => {
+        gsap.fromTo(el, 1, { opacity: 0, zIndex: -1 }, { opacity: 1, zIndex: 1 });
+    };
+
+    const _animateStepExit = (el) => {
+        gsap.fromTo(el, 1, { opacity: 1, zIndex: 1 }, { opacity: 0, zIndex: -1 });
+    };
 
     return (
         <div className = { Styles.form } >
-            { titleJSX }
+            <div className = { Styles.heading }>
+                <Transition
+                    in = { step !== 3 }
+                    timeout = { timeout }
+                    onExit = { _animateStepExit }>
+                    <h3>Signup</h3>
+                </Transition>
+
+                <Transition
+                    in = { step === 3 }
+                    timeout = { timeout }
+                    onEntered = { _animateStepEnter }>
+                    <h3>Thank you!</h3>
+                </Transition>
+            </div>
 
             <ProgressBar step = { step } />
 
-            { step === 1 && <FirstStep onSubmit = { stepForward } /> }
+            <div className = { Styles.wrapper }>
+                <Transition
+                    in = { step === 1 }
+                    timeout = { timeout }
+                    onEntered = { _animateStepEnter }
+                    onExit = { _animateStepExit }>
+                    <FirstStep onSubmit = { _stepForward } />
+                </Transition>
 
-            { step === 2 &&
-                <SecondStep
-                    actions = { { ...actions } }
-                    stepBackward = { stepBackward }
-                    onSubmit = { stepForward }
-                />
-            }
+                <Transition
+                    in = { step === 2 }
+                    timeout = { timeout }
+                    onEntered = { _animateStepEnter }
+                    onExit = { _animateStepExit }>
+                    <SecondStep
+                        actions = { { ...actions } }
+                        stepBackward = { _stepBackward }
+                        onSubmit = { _stepForward }
+                    />
+                </Transition>
 
-            { step === 3 && <ThirdStep onSubmit = { onSubmit } /> }
+                <Transition
+                    in = { step === 3 }
+                    timeout = { timeout }
+                    onEntered = { _animateStepEnter }
+                    onExit = { _animateStepExit }>
+                    <ThirdStep onSubmit = { onSubmit } />
+                </Transition>
+            </div>
+
+            <Transition
+                in = { step !== 3 }
+                timeout = { timeout }
+                onExit = { _animateStepExit }>
+                <div className = { Styles.line } />
+            </Transition>
         </div>
     );
 };
